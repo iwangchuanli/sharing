6fd12a8d50eb687420f74dd560bc1955240f13d0
wangchuanli001 <wangchuanli_@hotmail.com>
2019-05-05 15:24:47 +0800

    services

--- src/main/java/com/mtons/mblog/modules/service/impl/SecurityCodeServiceImpl.java
+++ src/main/java/com/mtons/mblog/modules/service/impl/SecurityCodeServiceImpl.java
@@ -1,12 +1,4 @@
-/*
-+--------------------------------------------------------------------------
-|   Mblog [#RELEASE_VERSION#]
-|   ========================================
-|   Copyright (c) 2014, 2015 mtons. All Rights Reserved
-|   http://www.mtons.com
-|
-+---------------------------------------------------------------------------
-*/
+
 package com.mtons.mblog.modules.service.impl;
 
 import com.mtons.mblog.base.lang.Consts;
