27fc181cf6b3b57ae50913a3463d680ad353d43e
wangchuanli001 <wangchuanli_@hotmail.com>
2019-06-04 12:35:29 +0800

    :+1:
    :100:
    
    Co-Authored-By: WangChuanli <wangchuanli001@users.noreply.github.com>

---
+++ classes/artifacts/sharing_war_exploded/src/main/java/com/mtons/mblog/modules/repository/SecurityCodeRepository.java
@@ -0,0 +1,14 @@
+
+package com.mtons.mblog.modules.repository;
+
+import com.mtons.mblog.modules.entity.SecurityCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+/**
+ * @author wangcl on 2015/8/14.
+ */
+public interface SecurityCodeRepository extends JpaRepository<SecurityCode, Long>, JpaSpecificationExecutor<SecurityCode> {
+    SecurityCode findByKeyAndType(String key, int type);
+    SecurityCode findByKey(String key);
+}
